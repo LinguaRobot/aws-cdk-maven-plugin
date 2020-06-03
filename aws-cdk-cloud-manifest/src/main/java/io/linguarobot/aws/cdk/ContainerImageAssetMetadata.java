51f4d96007f5e0edbcb76f7c1fbe4334ee33b77a
Roman Kishchenko <rkishchenko@gmail.com>
2020-06-03 18:25:38 +0300

    Add a module defining a model for cloud manifest deserialisation

---
+++ aws-cdk-cloud-manifest/src/main/java/io/linguarobot/aws/cdk/ContainerImageAssetMetadata.java
@@ -0,0 +1,22 @@
+package io.linguarobot.aws.cdk;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.List;
+
+@JsonPropertyOrder({"type", "data", "trace"})
+public class ContainerImageAssetMetadata extends AssetMetadata {
+
+    public ContainerImageAssetMetadata(
+            @JsonProperty("type") MetadataType type,
+            @JsonProperty("data") ContainerAssetData data,
+            @JsonProperty("trace") List<String> trace) {
+        super(type, data, trace);
+    }
+
+    public ContainerAssetData getData() {
+        return (ContainerAssetData) super.getData();
+    }
+
+}
---
+++ aws-cdk-cloud-manifest/src/main/java/io/linguarobot/aws/cdk/StackArtifact.java
@@ -0,0 +1,25 @@
+package io.linguarobot.aws.cdk;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.List;
+import java.util.Map;
+
+@JsonPropertyOrder({"type", "environment", "metadata", "dependencies", "properties"})
+public class StackArtifact extends Artifact {
+
+    public StackArtifact(
+            @JsonProperty("type") ArtifactType type,
+            @JsonProperty("environment") String environment,
+            @JsonProperty("metadata") Map<String, List<ArtifactMetadata>> metadata,
+            @JsonProperty("dependencies") List<String> dependencies,
+            @JsonProperty("properties") StackProperties properties) {
+        super(type, environment, metadata, dependencies, properties);
+    }
+
+    public StackProperties getProperties() {
+        return (StackProperties) super.getProperties();
+    }
+
+}
