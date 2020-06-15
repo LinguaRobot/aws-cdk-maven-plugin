87003253e49d3287b84f2a98543d3849521225ca
Roman Kishchenko <rkishchenko@gmail.com>
2020-06-15 11:47:36 +0300

    chore(deps): do not use SparkBase that doesn't exist in the newer version of Spark

--- aws-cdk-maven-plugin/src/it/synth-deploy-ecs-service/rest-service/src/main/java/io/linguarobot/aws/cdk/maven/it/RestApp.java
+++ aws-cdk-maven-plugin/src/it/synth-deploy-ecs-service/rest-service/src/main/java/io/linguarobot/aws/cdk/maven/it/RestApp.java
@@ -1,12 +1,12 @@
 package io.linguarobot.aws.cdk.maven.it;
 
 import static spark.Spark.get;
-import static spark.SparkBase.setPort;
+import static spark.Spark.port;
 
 public class RestApp {
 
     public static void main(String[] args) {
-        setPort(8080);
+        port(8080);
         get("/", (request, response) -> "SUCCESS");
     }
 
